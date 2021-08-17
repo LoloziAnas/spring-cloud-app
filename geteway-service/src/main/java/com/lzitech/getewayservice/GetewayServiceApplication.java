d6f46b4474b00dec0291b039822d9a849c16043d
LoloziAnas <lolozianas1@gmail.com>
2021-08-17 04:35:43 +0100

    adding gateway sevice

---
+++ geteway-service/src/main/java/com/lzitech/getewayservice/GetewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lzitech.getewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetewayServiceApplication.class, args);
+    }
+
+}
---
+++ geteway-service/src/test/java/com/lzitech/getewayservice/GetewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzitech.getewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
