643c772e21f3f17a8a1902a9e5e61ba36c515d1f
LoloziAnas <lolozianas1@gmail.com>
2021-08-18 04:32:17 +0100

    implementing new features

--- inventory-service/src/main/java/com/lzitech/inventoryservice/DAO/ProductRepository.java
+++ inventory-service/src/main/java/com/lzitech/inventoryservice/DAO/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.lzitech.inventoryservice.dao;
+package com.lzitech.inventoryservice.DAO;
 
 import com.lzitech.inventoryservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
--- inventory-service/src/main/java/com/lzitech/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/lzitech/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,7 @@
 package com.lzitech.inventoryservice;
 
-import com.lzitech.inventoryservice.dao.ProductRepository;
 import com.lzitech.inventoryservice.models.Product;
+import com.lzitech.inventoryservice.DAO.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
