0aa36361b24f4a8d49076d40484c2cd424e23c2a
LoloziAnas <lolozianas1@gmail.com>
2021-08-19 22:36:46 +0100

    implementing covid api

--- gateway-service/src/main/java/com/lzitech/getewayservice/contollers/CircuitBreakerController.java
+++ gateway-service/src/main/java/com/lzitech/getewayservice/contollers/CircuitBreakerController.java
@@ -1,4 +1,18 @@
 package com.lzitech.getewayservice.contollers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class CircuitBreakerController {
+    @GetMapping("/defaultCovidStatistic")
+    public Map<String,String> defaultCovidStatistic(){
+        Map<String,String> data = new HashMap<>();
+        data.put("msg","default Covid Statistic");
+
+        return data;
+    }
 }
